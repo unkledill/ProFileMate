6a15ac90578a328f76d41bc5dc6f434f44b77f7d
Malik Abayomi <brh.abdulmalik@gmail.com>
2023-09-18 12:58:34 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         textTheme: GoogleFonts.merriweatherTextTheme(),
         useMaterial3: true,
